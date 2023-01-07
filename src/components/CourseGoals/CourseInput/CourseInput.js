6ac9dbf53ab3b85a289bccc0ee988dc0827ab5df
naekit <mykyta.onishchuck@code.berlin>
2023-01-07 12:15:52 -0500

    various choices for styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
+import styled from "styled-components"
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button"
+import styles from "./CourseInput.module.css"
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// const FormControl = styled.div`
+// 	margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+// 	& label {
+// 		font-weight: bold;
+// 		display: block;
+// 		margin-bottom: 0.5rem;
+// 		color: ${(props) => (props.invalid ? "red" : "black")};
+// 	}
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+// 	& input {
+// 		display: block;
+// 		width: 100%;
+// 		border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+// 		background: ${(props) => (props.invalid ? "rgb(235, 222, 222)" : "")};
+// 		font: inherit;
+// 		line-height: 1.5rem;
+// 		padding: 0 0.25rem;
+// 	}
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
-};
+// 	& input:focus {
+// 		outline: none;
+// 		background: #fad0ec;
+// 		border-color: #8b005d;
+// 	}
+// `
 
-export default CourseInput;
+const CourseInput = (props) => {
+	const [enteredValue, setEnteredValue] = useState("")
+	const [valid, setValid] = useState(true)
+
+	const goalInputChangeHandler = (event) => {
+		setEnteredValue(event.target.value)
+		setValid(true)
+	}
+
+	const formSubmitHandler = (event) => {
+		event.preventDefault()
+		if (enteredValue.trim().length === 0) {
+			setValid(false)
+			return
+		}
+		props.onAddGoal(enteredValue)
+		setEnteredValue("")
+	}
+
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<div
+				className={`${styles["form-control"]} ${
+					!valid && styles.invalid
+				}`}
+			>
+				<label>Course Goal</label>
+				<input
+					value={enteredValue}
+					type="text"
+					onChange={goalInputChangeHandler}
+				/>
+			</div>
+			<Button type="submit">Add Goal</Button>
+		</form>
+	)
+}
+
+export default CourseInput
